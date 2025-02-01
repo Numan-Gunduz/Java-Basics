99016be1824ebbe09728d33e71fa9eeedfbd40b7
Numan GÜNDÜZ <129600548+Numan-Gunduz@users.noreply.github.com>
2025-02-01 14:13:45 +0300

    intelij idea

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Merhaba Dünya");
+        //küçük büyük harf duyarlılığı vardır.
+        //github deneme adımı
+
+    }
+}
