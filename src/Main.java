e576e166e9226ae17532401eb1dc45b8a27f3c34
Numan GÜNDÜZ <129600548+Numan-Gunduz@users.noreply.github.com>
2025-02-01 14:49:18 +0300

    variables

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,27 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Merhaba Dünya");
-        //küçük büyük harf duyarlılığı vardır.
-        //github deneme adımı
-        //ikinci deneme intelij ide github configuration
+
+        int a = 4 , b =4;
+        String name = "name";
+        System.out.println("Benim ismim ne biliyor musun ? " + name );
+
+        int toplam = 0 ;
+        toplam  = a+b;
+        System.out.println("iki sayının toplamı : " + toplam);
+        System.out.println(name+toplam);
+
 
     }
 }
+/*
+string c# farklı olarak büyük harf ile yazılyırore
+java da yazım kurallar:
+Türkçe karakter kullanımı ;
+-java da sınıf adlarının ilk harfi her zaman büyük harf ile başlamalıdır
+*eğer iki kelimeli bir sınıf adı kullanıyorsak onu da böyle yazmamız gerekiyor CarsMoodels
+*metot adları her zaman küçük harf ile başlanmalıdır. Eğer iki kelime ise ilki küçük sonraki Büyük yazılır .
+*örneğin bmwCar();
+**yani buraya kadar sınıf adaları büyük metot adları ise küçük yazılır. Yani Camel Case:)
+
+* */
