4aa7a39f38a3a79b111c428b97425490bfec46b3
Numan GÜNDÜZ <129600548+Numan-Gunduz@users.noreply.github.com>
2025-02-04 15:07:51 +0300

    Methods

---
+++ src/AccessModifiers.java
@@ -0,0 +1,14 @@
+public class AccessModifiers
+{
+    public static void main(String[] args) {
+
+
+        /*
+        * Access Modifiers
+        * 1) Default Modifiers : SAdece aynı java paketi içinde görünür.
+        * 2) Public Modifiers : Açık ve tüm dünyanın kullanımına sunulabilir. En az kısıtlayıcı olmayan erişim belirleyicidir.
+        * 3)Private Modifiers : Yalnızca aynı sınıftan yani class'tan erişilebilir. En kısıtlayıcı erişm belirleyicidir.Encapsulation ile ilişkilidir.
+        * 4)Protected Modifiers : Protected üyeye aynı paketten ve tüm alt sınıflardan erişim sağlanabilir. Defaulttan farklı olarak alt sınıflardan da erişilebilir olmasından kaynaklıdır.
+        * */
+    }
+}
---
+++ src/Methods.java
@@ -0,0 +1,32 @@
+public class Methods {
+
+
+    public static  void main(String[] args) {
+
+boolean ısControl = true ;
+if(ısControl)
+{
+     Toplam(2,5);
+     Selamla();
+}
+
+    }
+
+    public static void Toplam( int i ,int j)
+    {
+        int toplam = 0 ;
+        toplam = i+j;
+        System.out.println(toplam);
+    }
+public  static void Selamla()
+{
+    System.out.println("Hello World");
+}
+
+
+
+}
+/*
+*
+* Void anahtar kelimesi herhangi bir dönüş tipi olamıdğını belirtir.
+* */
